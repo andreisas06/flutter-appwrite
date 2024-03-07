8387b4b709ca6ec4e2b54654df25bb2ddd9b469d
Andrei <andrei@baseflow.com>
2024-03-07 12:10:35 +0100

    added appwrite

---
+++ lib/src/data/constants.dart
@@ -0,0 +1,14 @@
+/// The class contains the constants used in the app.
+class Appconstants {
+  /// The project id of the appwrite project.
+  static const String projectid = '65e1eb99e55575c5b863';
+
+  /// The endpoint of the appwrite project.
+  static const String endpoint = 'https://cloud.appwrite.io/v1';
+
+  /// The id of the database.
+  static const String dbID = '65e4f7c0eb74d20590b1';
+
+  /// The id of the collection.
+  static const String collectionID = '65e4f7e53e66f23287bf';
+}
